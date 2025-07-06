e331f27cc58da9b509b8c201816346c3f47fe895
Rémi <doundohouremi@gmail.com>
2025-07-06 12:11:18 +0000

    Commit 06072025

---
+++ ManageStock/Controllers/DashboardController.cs
@@ -0,0 +1,45 @@
+﻿using ManageStock.Data.Services.Stock;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManageStock.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly IStockService _StockService;
+
+        public DashboardController(IStockService StockService)
+        {
+            _StockService = StockService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetStockChart()
+        {
+            var produits = await _StockService.GetAll(); // ex: liste de produits avec Nom et Quantité
+
+
+            var chartData = new
+            {
+                labels = produits.Select(p => p.IdProduitNavigation.Nom).ToArray(),
+                datasets = new[]
+                {
+            new {
+                data = produits.Select(p => p.QuantitéDisponible).ToArray(),
+                backgroundColor = produits.Select(p => GetRandomColor()).ToArray()
+                }
+            }
+            };
+
+            return Json(chartData);
+        }
+
+        private string GetRandomColor()
+        {
+            var rand = new Random();
+            return $"#{rand.Next(0x1000000):X6}";
+        }
+    }
+}
---
+++ ManageStock/wwwroot/js/Stocks.js
@@ -0,0 +1,14 @@
+﻿fetch('/Dashboard/GetStockChart')
+    .then(response => response.json())
+    .then(data => {
+        new Chart(document.getElementById('stockChart'), {
+            type: 'bar',
+            data: data,
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: { display: false } // facultatif si tu veux aussi cacher la légende
+                }
+            }
+        });
+    });
